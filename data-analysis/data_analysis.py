f82f01b698651d82e262d5d71531954353a05998
giacThePhantom <giacomo.fantoni@studenti.unitn.it>
2023-01-10 04:05:53 +0100

    Block analysis

--- data-analysis/data_analysis.py
+++ data-analysis/data_analysis.py
@@ -9,6 +9,7 @@ import pandas as pd
 import plotly.express as px
 import MDAnalysis as mda
 from MDAnalysis.lib import distances
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
 
 def all_to_all_rmsd(filename, outname):
     rmsd_data = pd.read_csv(filename, sep=',', header = None)
@@ -50,3 +51,41 @@ def contact_map(gro, xtc, outname, sel = "name CA"):
     plt.colorbar(img)
     plt.savefig(outname)
     plt.clf()
+
+def clustering(filename, outname, linkage_method = 'average'):
+    rmsd_data = pd.read_csv(filename, sep=',', header = None)
+    dim = max(rmsd_data[1])+1
+    rmsd_map = np.array(rmsd_data[2]).reshape((dim,dim))
+    Z = linkage(rmsd_map, linkage_method)
+    dn = dendrogram(Z)
+    plt.savefig(outname)
+    plt.clf()
+
+def block_analysis(filename, outname, increase = 10, ds = 5):
+    data = pd.read_csv(filename, sep = ",", header = None)
+    dim = max(data[1]) + 1
+    dim_block = 0
+    bse = []
+    correlation_time = []
+    while dim_block <= dim/2:
+        dim_block += increase
+        nblocks = int(np.ceil(dim/dim_block))
+        means = []
+        for i in range(nblocks):
+            block = data.loc[(data[0] >= (i*dim_block)) &
+                            (data[0] < (i+1)*dim_block) &
+                            (data[1] >= (i*dim_block)) &
+                            (data[1] < (i+1)*dim_block) &
+                             data[2] != 0]
+            if len(block) != 0:
+                means.append(np.mean(block.iloc[:,2]))
+            else:
+                nblocks -=1
+        bse.append(np.std(means)/np.sqrt(nblocks))
+        correlation_time.append(dim_block * ds)
+        plt.ylabel("BSE")
+        plt.xlabel("Correlation time")
+        plt.title("Block analysis")
+        plt.plot(correlation_time, bse)
+        plt.savefig(outname)
+        plt.clf()
